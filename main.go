22d582a07485164d927773f724f2a0078e45f7a7
militska <militska@intersvyaz.net>
2020-10-10 20:02:30 +0500

    add net/http,  добавила рест апи метод, для отправки почты

--- main.go
+++ main.go
@@ -1,28 +1,72 @@
 package main
 
 import (
+	"encoding/json"
+	//"encoding/json"
+	"io/ioutil"
+
+	//"fmt"
 	"github.com/joho/godotenv"
+	//"html"
 	"log"
+	"net/http"
+	"os"
+	"time"
 )
+
+const ENABLED = "1"
 
 func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Print("No .env file found")
 	}
+
+	if os.Getenv("ENABLED_API") == ENABLED {
+		s := &http.Server{
+			Addr:           ":8070",
+			ReadTimeout:    10 * time.Second,
+			WriteTimeout:   10 * time.Second,
+			MaxHeaderBytes: 1 << 20,
+		}
+
+		http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				panic(err)
+			}
+			var t SendEmailDTO
+			err = json.Unmarshal(body, &t)
+			if err != nil {
+				panic(err)
+			}
+			log.Println(t.Body)
+
+			send_msg(t)
+		})
+
+		log.Fatal(s.ListenAndServe())
+	}
+
 }
 
-func send_msg() {
+type SendEmailDTO struct {
+	From         string
+	FromExtended string
+	To           []string
+	Subject      string
+	Body         string
+}
+
+func send_msg(dto SendEmailDTO) {
 	authStr := AuthStr{}
 	auth := authStr.getData()
 
-	to := []string{"cheshirenok@gmail.com"}
-	from := "militska.ru@gmail.com"
+	to := dto.To
+	from := dto.From
 	msg := Msg{
-		From: from,
-		//From:    "test change <militska.ru@gmail.com>",
-		To:      "",
-		Subject: "test title 333",
-		Body:    "test body 222",
+		From:    dto.FromExtended,
+		Subject: dto.Subject,
+		Body:    dto.Body,
 	}
 
 	text, _ := msg.getText()
--- smtp_server.go
+++ smtp_server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/smtp"
+	"os"
 )
 
 type SmtpServer struct {
