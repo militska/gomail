dc97826cd493af7191c20aafb57edb5f4f303cbb
militska <militska@intersvyaz.net>
2020-10-10 18:36:07 +0500

    прибралась

--- main.go
+++ main.go
@@ -3,21 +3,23 @@ package main
 import (
 	"github.com/joho/godotenv"
 	"log"
-	"net/smtp"
-	"os"
 )
 
 func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Print("No .env file found")
 	}
+}
 
+func send_msg() {
 	authStr := AuthStr{}
 	auth := authStr.getData()
-	to := []string{"cheshirenok@gmail.com"}
 
+	to := []string{"cheshirenok@gmail.com"}
+	from := "militska.ru@gmail.com"
 	msg := Msg{
-		From:    "test change <militska.ru@gmail.com>",
+		From: from,
+		//From:    "test change <militska.ru@gmail.com>",
 		To:      "",
 		Subject: "test title 333",
 		Body:    "test body 222",
@@ -28,40 +30,8 @@ func main() {
 	server := SmtpServer{
 		Auth:    auth,
 		To:      to,
-		From:    os.Getenv("EMAIL_USER") + "@gmail.com",
+		From:    from,
 		Message: text,
 	}
 	server.send()
-}
-
-type SmtpServer struct {
-	Auth    smtp.Auth
-	Host    string
-	Port    int
-	To      []string
-	From    string
-	Message []byte
-}
-
-func (server *SmtpServer) send() {
-	err := smtp.SendMail(os.Getenv("HOST")+":"+os.Getenv("PORT_SMTP"),
-		server.Auth,
-		server.From,
-		server.To,
-		server.Message,
-	)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-type AuthStr struct{}
-
-func (a *AuthStr) getData() smtp.Auth {
-	data := smtp.PlainAuth("",
-		os.Getenv("EMAIL_USER"),
-		os.Getenv("EMAIL_PASSWORD"),
-		os.Getenv("HOST"))
-	return data
 }
---
+++ smtp_server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net/smtp"
+)
+
+type SmtpServer struct {
+	Auth    smtp.Auth
+	Host    string
+	Port    int
+	To      []string
+	From    string
+	Message []byte
+}
+
+func (server *SmtpServer) send() {
+	err := smtp.SendMail(os.Getenv("HOST")+":"+os.Getenv("PORT_SMTP"),
+		server.Auth,
+		server.From,
+		server.To,
+		server.Message,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type AuthStr struct{}
+
+func (a *AuthStr) getData() smtp.Auth {
+	data := smtp.PlainAuth("",
+		os.Getenv("EMAIL_USER"),
+		os.Getenv("EMAIL_PASSWORD"),
+		os.Getenv("HOST"))
+	return data
+}
