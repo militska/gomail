baa58775de81f7eda718543f41d69f501cbf703d
militska <militska@intersvyaz.net>
2020-10-10 11:42:45 +0500

    add struct

--- main.go
+++ main.go
@@ -17,12 +17,29 @@ func main() {
 	auth := smtp.PlainAuth("", os.Getenv("EMAIL_USER"), os.Getenv("EMAIL_PASSWORD"), os.Getenv("HOST"))
 
 	to := []string{"cheshirenok@gmail.com"}
-	msg := []byte("To: cheshirenok@gmail.com\r\n" +
-		"Subject: discount Gophers!\r\n" +
-		"\r\n" +
-		"This is the email body.\r\n")
-	err := smtp.SendMail(os.Getenv("HOST")+":587", auth, os.Getenv("EMAIL_USER")+"@gmail.com", to, msg)
+
+	msg := Msg{From: "test change <militska.ru@gmail.com>", To: "", Subject: "test title", Body: "test body"}
+
+	err := smtp.SendMail(os.Getenv("HOST")+":587", auth, os.Getenv("EMAIL_USER")+"@gmail.com", to, msg.getText())
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+type Msg struct {
+	To      string
+	From    string
+	Subject string
+	Body    string
+}
+
+func (m *Msg) getText() []byte {
+
+	message := []byte(
+		"To: " + m.To + "\r\n" +
+			"From: " + m.From + "\r\n" +
+			"Subject: " + m.Subject + "\r\n" +
+			"\r\n" + m.Body + "\r\n")
+
+	return message
 }
